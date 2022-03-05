9a044daa9b011fcfe89c930432d63602985e27c5
Miguel Ccantuta <mccantuta@yahoo.com>
2022-03-05 11:31:55 -0500

    Packages

---
+++ src/1.Basics/packages/withoutMainFunc.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// command go run fail because there is not any main function declared
+func anyFunction() {
+	fmt.Println("Any function")
+}
---
+++ src/1.Basics/packages/withoutMainPackage.go
@@ -0,0 +1,8 @@
+package anyPackage
+
+import "fmt"
+
+// go run command fail because there is not a main package
+func main() {
+	fmt.Println("Main function")
+}
