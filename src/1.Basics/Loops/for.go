499fd227d698bcede89ed3000c011a26ab813072
Miguel Ccantuta <mccantuta@yahoo.com>
2022-03-07 08:01:49 -0500

    For loops

---
+++ src/1.Basics/Loops/for.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	forWithInit()
+	forLikeWhile(1)
+	forInfinite(1)
+}
+
+func forWithInit() {
+	// for init; condition; post {}
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+	}
+}
+
+func forLikeWhile(i int) {
+	// for condition {}
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}
+}
+
+func forInfinite(i int) {
+	// for {}
+	for {
+		fmt.Println(i)
+		i++
+		if i > 5 {
+			break
+		}
+
+	}
+}
